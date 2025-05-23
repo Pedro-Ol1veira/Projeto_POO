410d51f8eace49f1e02fbb543c630e5a3b852b42
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2025-05-23 02:23:12 -0300

    Configura o React-Router-Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterStudent from "./Components/RegisterStudent/RegisterStudent";
 
+function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RegisterStudent />}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
