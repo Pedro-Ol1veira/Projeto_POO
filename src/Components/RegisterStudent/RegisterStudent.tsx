fe21f4c0e060509ec2512235d2ac37600a65ad35
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2025-06-02 19:53:38 -0300

    Feat: Adiciona os campos area de concentracao e nota do enem ao formulario de registro

--- src/Components/RegisterStudent/RegisterStudent.tsx
+++ src/Components/RegisterStudent/RegisterStudent.tsx
@@ -12,7 +12,15 @@ export default function RegisterStudent() {
         <input type="text" id="course" />
       </div>
       <div className={style.formGroup}>
-        <label htmlFor="yearEntry">Ano de ingresso:</label>
+        <label htmlFor="ac">Aréa de concetração:</label>
+        <input type="text" id="ac" />
+      </div>
+      <div className={style.formGroup}>
+        <label htmlFor="enem">Nota do Enem:</label>
+        <input type="number" id="enem" />
+      </div>
+      <div className={style.formGroup}>
+        <label htmlFor="yearEntry">Semestre de ingresso:</label>
         <input type="text" id="yearEntry" />
       </div>
       <button type="submit">Registrar</button>
