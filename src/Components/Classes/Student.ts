afd2162749dea915598f372d4cdd8640fdfa5f86
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2025-06-02 19:30:02 -0300

    Feat: Criando classe de aluno

---
+++ src/Components/Classes/Student.ts
@@ -0,0 +1,28 @@
+class Student {
+    private materias: Array<String>;
+    private matricula: string;
+    private ac: string;
+    private curso: string;
+    private semIng: string;
+    private notaDoEnem: number;
+
+    constructor(materias: Array<String>, matricula: string, ac:string, curso: string, semIng: string, notaDoEnem: number) {
+        this.materias = materias;
+        this.matricula = matricula;
+        this.ac = ac;
+        this.curso = curso;
+        this.semIng = semIng;
+        this.notaDoEnem = notaDoEnem;
+    }
+
+    public cadastrarMateria(newMateria: string) {
+        this.materias.push(newMateria);
+    }
+
+    public trancarMateria(materiaTrancada: string) {
+        this.materias = this.materias.filter((materia) => {
+            return materia != materiaTrancada;
+        });
+    }
+
+}
