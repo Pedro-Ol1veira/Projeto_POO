c5429a9de87ef07b82ba8a469189621efa16aa0b
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2025-06-15 22:50:56 -0300

    Feat: Cria HTML do formulario de adicionar uma materia

---
+++ src/Components/Dashboard/Dashboard.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import SubjectForm from "../SubjectForm/SubjectForm";
+import styles from './Dashboard.module.css';
+import { useStudent } from "../../Context/studentContext";
+import type Subject from "../Classes/Subject";
+
+
+export default function Dashboard () {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const {student} = useStudent();
+
+  const handleFormSuccess = () => {
+    setIsModalOpen(false);
+  }
+
+  return (
+    <div className={styles.dashboard_container}>
+      <header className={styles.headerContainer}>
+        <h1 className={styles.title}>Meu Dashboard</h1>
+        <button onClick={() => setIsModalOpen(true)} className={styles.addButton}>
+          + Adicionar Materia
+        </button>
+      </header>
+
+      {isModalOpen && (
+        <div className={styles.modal_overlay}>
+          <div className={styles.modal_content}>
+            <button onClick={() => setIsModalOpen(false)} className={styles.close_btn}>
+              &times;
+            </button>
+            <SubjectForm onSuccess={handleFormSuccess}/>
+          </div>
+        </div>
+      )}
+
+      {student && student.getMaterias().map((subject: Subject) => (
+        <div key={subject.getId()}>
+          <p>{subject.getCodigo()}</p>
+          <p>{subject.getNome()}</p>
+          <p>{subject.getDias()}</p>
+          <p>{subject.getHorario()}</p>
+          <p>{subject.getNota()}</p>
+          <p>{subject.getTasks()}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/Components/SubjectForm/SubjectForm.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import styles from './SubjectForm.module.css';
+import { useStudent } from '../../Context/studentContext';
+
+interface IProps {
+  onSuccess: () => void;
+}
+
+export default function SubjectForm ({ onSuccess }: IProps) {
+  const { student } = useStudent();
+  const DIAS_DA_SEMANA = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+  const [code, setCode] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [time, setTime] = useState<string>("");
+  const [dates, setDates] = useState<string[]>([]);
+  
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(student);
+    onSuccess();
+  }
+
+  const handleDiaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+
+    if (checked) {
+      setDates(prevState => [...prevState, name]);
+    }
+  };
+  
+  return (
+    <form className={styles.formContainer} onSubmit={handleSubmit}>
+      <div className={styles.formGroup}>
+        <label htmlFor="code" className={styles.formLabel}>Código</label>
+        <input type="text" id="code" name="code" className={styles.formInput} onChange={(e) => setCode(e.target.value)}/>
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="name" className={styles.formLabel}>Nome</label>
+        <input type="text" id="name" name="name" className={styles.formInput} onChange={(e) => setName(e.target.value)} />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label htmlFor="time" className={styles.formLabel}>Horário</label>
+        <input type="text" id="time" name="time" className={styles.formInput} placeholder="Ex: 14:00 - 16:00" onChange={(e) => setTime(e.target.value)}/>
+      </div>
+
+      <div className={styles.formGroup}>
+        <label className={styles.formLabel}>Dias</label>
+        <div className={styles.checkboxGroupContainer}>
+          {DIAS_DA_SEMANA.map((dia) => (
+            <div key={dia} className={styles.checkboxItem}>
+              <input
+                type="checkbox"
+                id={dia}
+                name={dia}
+                checked={dates.includes(dia)}
+                onChange={handleDiaChange}
+                className={styles.checkboxInput}
+              />
+              <label htmlFor={dia} className={styles.checkboxLabel}>{dia}</label>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <button type="submit" className={styles.submitButton}>
+        Enviar
+      </button>
+    </form>
+  );
+}
